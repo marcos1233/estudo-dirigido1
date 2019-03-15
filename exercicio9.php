5adff47363ab61b7f5ad962ee5806528e0e82c3a
Marcos Alexandre Soldati Quandt <48591167+marcos1233@users.noreply.github.com>
2019-03-15 16:05:41 -0300

    Update exercicio9.php

--- exercicio9.php
+++ exercicio9.php
@@ -1,12 +1,50 @@
 <?php
 
-print "Qual o tamanho da área a ser pintada,em metros quadrados,:";
-$area=(float)fgets(STDIN);
+print "\nQual area você deseja pintar: ";
+$area = (int)fgets(STDIN);
 
-$totallitros=($area/6)*1.1;
-$nlatas=$totallitros/18;
-$precolatas=$nlatas*80;
-$ngaloes=$totallitros/3.6;
-$precogaloes=$ngaloes*25;
+#lata
+    $lata_rend = 108; #18*6
+    $lata_preco = 80;
+#a  compra apenas de latas de 18 litros
+    $qtd_latas = ceil($area / $lata_rend);
+    $latas_custo = $qtd_latas * $lata_preco;
+     print "\nSerão necessárias $qtd_latas lata(s) no valor de R$$latas_custo\n";
 
-$nlatasgaloes=($totallitros/18);
+<?php
+
+print "\nQual area você deseja pintar: ";
+$area = (int)fgets(STDIN);
+
+#galao
+    $galao_rend = 21.6; #3.6*6
+    $galao_preco = 25;
+#a  compra apenas de galaoes de 3,6 litors
+    $qtd_galao = ceil($area / $galao_rend);
+    $galao_custo = $qtd_galao * $galao_preco;
+     print "\nSerão necessárias $qtd_galao lata(s) no valor de R$$galao_custo\n";
+
+<?php
+//Exercicio1
+
+print "\nQual area você deseja pintar: ";
+$area = (int)fgets(STDIN);
+    
+#lata
+    $lata_rend = 108; #18*6
+    $lata_preco = 80;
+#galao
+    $galao_rend = 21.6; #3.6*6
+    $galao_preco = 25;    
+#a  compra apenas de latas de 18 litros
+    $qtd_latas = ceil($area / $lata_rend);
+    $latas_custo = $qtd_latas * $lata_preco;
+print "\nSerão necessárias $qtd_latas lata(s) no valor de R$$latas_custo\n";
+    $qtd_latas = 0;
+    $qtd_galoes= 0;
+    $qtd_latas = (int) ($area / $lata_rend);
+    $faltou_latas = $area % $lata_rend;
+    $qts_galoes = ceil($faltou_latas / $galao_rend);
+
+    print "$faltou_latas - $qtd_latas\n";
+
